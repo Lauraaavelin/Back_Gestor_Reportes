c7d054cfc6ac670466da6eea6531901b265fda5d
Laura Avelino <l.avelino@uniandes.edu.co>
2026-05-06 15:15:52 -0500

    url para mostrar la vista y no solo un test

--- Gestor/ataque/urls.py
+++ Gestor/ataque/urls.py
@@ -1,6 +1,7 @@
+# ataque/urls.py
 from django.urls import path
-from django.http import HttpResponse
+from . import views
 
 urlpatterns = [
-    path('configuracion-sensible/', lambda r: HttpResponse("test")),
+    path('configuracion-sensible/', views.ruta_critica_view, name='sensible'),
 ]
